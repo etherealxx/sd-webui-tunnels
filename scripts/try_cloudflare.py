fea38ea3530f0dcdf9a60e4f87da0b048e294831
Etherealxx <64251396+etherealxx@users.noreply.github.com>
2023-03-03 14:58:06 +0700

    Supress the warning if fails and print a more noticeable message

--- scripts/try_cloudflare.py
+++ scripts/try_cloudflare.py
@@ -9,8 +9,12 @@ import os
 if cmd_opts.cloudflared:
     print("cloudflared detected, trying to connect...")
     port = cmd_opts.port if cmd_opts.port else 7860
-    tunnel_url = try_cloudflare(port=port, verbose=False)
-    os.environ['webui_url'] = tunnel_url.tunnel
-    colab_url = os.getenv('colab_url')
-    strings.en["SHARE_LINK_MESSAGE"] = f"Public WebUI Colab URL: {tunnel_url.tunnel}"
+    try:
+        tunnel_url = try_cloudflare(port=port, verbose=False)
+        os.environ['webui_url'] = tunnel_url.tunnel
+        colab_url = os.getenv('colab_url')
+        strings.en["SHARE_LINK_MESSAGE"] = f"Public WebUI Colab URL: {tunnel_url.tunnel}"
+    except RuntimeError:
+        print("[1;33mCloudflared failed to start. Use the public link instead.") #yellow color
+        print('[0m') #revert the color back to default
     
