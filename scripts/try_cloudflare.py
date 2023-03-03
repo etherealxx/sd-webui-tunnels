bcd0d7cde19dc5c750f86ab6b8d096f8e6e3a429
Etherealxx <64251396+etherealxx@users.noreply.github.com>
2023-03-03 14:59:22 +0700

    try-except

--- scripts/try_cloudflare.py
+++ scripts/try_cloudflare.py
@@ -9,7 +9,10 @@ import os
 if cmd_opts.cloudflared:
     print("cloudflared detected, trying to connect...")
     port = cmd_opts.port if cmd_opts.port else 7860
-    tunnel_url = try_cloudflare(port=port, verbose=False)
-    os.environ['webui_url'] = tunnel_url.tunnel
-    colab_url = os.getenv('colab_url')
-    strings.en["SHARE_LINK_MESSAGE"] = f"Public WebUI Colab URL: {tunnel_url.tunnel}"
+    try:
+        tunnel_url = try_cloudflare(port=port, verbose=False)
+        os.environ['webui_url'] = tunnel_url.tunnel
+        colab_url = os.getenv('colab_url')
+        strings.en["SHARE_LINK_MESSAGE"] = f"Public WebUI Colab URL: {tunnel_url.tunnel}"
+    except RuntimeError:
+        print("Cloudflared failed to start. Use the public link instead.")
